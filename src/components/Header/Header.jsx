8b67069b0d72c26a3c6427f259f04282b1f02933
unknown <gausulislam5@gmail.com>
2023-10-11 14:34:43 +0600

    update navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is a header</h1>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Blog</a>
+            <a href="">Inventory</a>
+            <a href="">Login</a>
+        </div>
+    );
+};
+
+export default Header;
